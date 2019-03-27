bbd58efb2e47a093a3c3cf4f51493e2339354314
Veison Ye <veison.ye@gpayroll.com>
2019-03-27 15:41:35 +0800

    commit change websecurity
    Change-Id: I0d8e326e40578f8c9d7bb08c0ae6ff68822c7be3

--- springboot-springSecurity3/src/main/java/com/us/example/config/WebSecurityConfig.java
+++ springboot-springSecurity3/src/main/java/com/us/example/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,41 +17,35 @@ import org.springframework.security.web.access.intercept.FilterSecurityIntercept
  * Created by yangyibo on 17/1/18.
  */
 
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private MyFilterSecurityInterceptor myFilterSecurityInterceptor;
+	@Autowired
+	private MyFilterSecurityInterceptor myFilterSecurityInterceptor;
 
+	@Bean
+	UserDetailsService customUserService() { // 注册UserDetailsService 的bean
+		return new CustomUserService();
+	}
 
-    @Bean
-    UserDetailsService customUserService() { //注册UserDetailsService 的bean
-        return new CustomUserService();
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(customUserService()); // user Details Service验证
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserService()); //user Details Service验证
+	}
 
-    }
+	@Override
+	public void configure(WebSecurity web) throws Exception { // 解决静态资源被拦截的问题
+		web.ignoring().antMatchers("/static/**");
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated() //任何请求,登录后可以访问
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .failureUrl("/login?error")
-                .permitAll() //登录页面用户任意访问
-                .and()
-                .logout().permitAll(); //注销行为任意访问
-        http.addFilterBefore(myFilterSecurityInterceptor, FilterSecurityInterceptor.class)
-                .csrf().disable();
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/css/**","/pictures/**","/js/**","/pages/**","/html/**").permitAll().anyRequest().authenticated() // 任何请求,登录后可以访问
+				.and().formLogin().loginPage("/login").failureUrl("/login?error").permitAll() // 登录页面用户任意访问
+				.and().logout().permitAll(); // 注销行为任意访问
+		http.addFilterBefore(myFilterSecurityInterceptor, FilterSecurityInterceptor.class).csrf().disable();
 
-
-    }
+	}
 }
-
